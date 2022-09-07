0a150e4796ac922b9006b69c47288b610ff29d8b
decabeza <alberto@decabeza.es>
2022-09-07 20:04:15 +0200

    Add frysk locale

--- config/initializers/application_custom.rb
+++ config/initializers/application_custom.rb
@@ -1,10 +1,10 @@
 module Consul
   class Application < Rails::Application
     unless Rails.env.test?
-      config.i18n.default_locale = :nl
-      config.i18n.available_locales = [:nl]
+      config.i18n.default_locale = "nl"
+      config.i18n.available_locales = ["nl", "fy-NL"]
       config.i18n.enforce_available_locales = false
-      config.i18n.fallbacks = { nl: :en }
+      config.i18n.fallbacks = { "nl" => "en", "fy-NL" => "nl" }
     end
   end
 end
